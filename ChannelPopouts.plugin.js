9b9ace2180210f3a80b2cc400911ca4b3618fe19
CompletelyUnbelievable <39345185+CompletelyUnbelievable@users.noreply.github.com>
2019-04-28 00:04:46 -0700

    Derp
    Change the version number so if it merges users will get the update.

--- ChannelPopouts.plugin.js
+++ ChannelPopouts.plugin.js
@@ -72,7 +72,7 @@ const ChannelPopoutRemoveHTML = function removeHTML(){
 
 
 var ChannelPopouts = (() => {
-    const config = {"info":{"name":"ChannelPopouts","authors":[{"name":"Green","discord_id":"80593258903773184","github_username":"Curtis-D"}],"version":"1.1.3","description":"Allows you to popout DMs/Servers to view more than one DM/Server at a time.","github":"","github_raw":"https://raw.githubusercontent.com/Curtis-D/ChannelPopouts/master/ChannelPopouts.plugin.js"},"main":"index.js"};
+    const config = {"info":{"name":"ChannelPopouts","authors":[{"name":"Green","discord_id":"80593258903773184","github_username":"Curtis-D"}],"version":"1.1.4","description":"Allows you to popout DMs/Servers to view more than one DM/Server at a time.","github":"","github_raw":"https://raw.githubusercontent.com/Curtis-D/ChannelPopouts/master/ChannelPopouts.plugin.js"},"main":"index.js"};
 
     return !global.ZeresPluginLibrary ? class {
         getName() {return config.info.name;} getAuthor() {return config.info.authors.map(a => a.name).join(", ");} getDescription() {return config.info.description;} getVersion() {return config.info.version;}
