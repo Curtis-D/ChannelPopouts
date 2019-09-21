db4b8b0fd1fd87c726d0280f229be1a5dc0d99fc
MasicoreLord <personaldesignertechie@gmail.com>
2019-09-21 18:02:56 -0400

    Bug Fixes
    - Fixed tooltip position on default theme
    - Fixed bug where the channel popouts button didn't appear if there were unread pins
    - Fixed bug where the servers and channels showed in the popout

--- ChannelPopouts.plugin.js
+++ ChannelPopouts.plugin.js
@@ -3,7 +3,7 @@
 function ChannelPopoutOnMouseEnter(){
     let wrapper = document.createElement('div');
     let buttonLeft = parseInt(document.getElementsByName('ChannelPopout')[0].getBoundingClientRect().left) - 36;
-    let buttonTop = parseInt(document.getElementsByName('ChannelPopout')[0].getBoundingClientRect().top) + 25;
+    let buttonTop = parseInt(document.getElementsByName('ChannelPopout')[0].getBoundingClientRect().top) + 32;
     wrapper.innerHTML = `<div class='layer-v9HyYc da-layer ChannelPopoutIcon' style='left: ` + buttonLeft.toString() + `px; top: ` + buttonTop.toString() + `px;'><div class="tooltip-2QfLtc tooltipBottom-3ARrEK tooltipBlack-PPG47z"><div class="tooltipPointer-3ZfirK da-tooltipPointer"></div>Popout Chat</div></div>`;
     document.querySelector('.layerContainer-yqaFcK').appendChild(wrapper.firstChild);
 };
@@ -24,8 +24,10 @@ function ChannelPopoutOnMouseClick(){
         });
     }
     win.webContents.once('did-finish-load', () => {
-        win.webContents.executeJavaScript(`document.querySelector('.channels-Ie2l6A').style.display = 'none';
-        document.querySelector('.wrapper-1Rf91z').style.display = 'none';`);
+        // Hides server and channel list along w/ ensuring the chat itself takes up the full width of the window
+        win.webContents.executeJavaScript(`document.querySelector('.sidebar-2K8pFh').style.display = 'none';
+        document.querySelector('.wrapper-1Rf91z').style.display = 'none';
+        document.querySelector('.base-3dtUhz').style.left = '0'`);
         if(isMac){
             win.webContents.executeJavaScript('document.getElementsByClassName("macButtonClose-MwZ2nf")[0].addEventListener("click", _ => {const w = require("electron").remote.getCurrentWindow(); w.close(); w.destroy();})');
         }
@@ -53,7 +55,7 @@ const ChannelPopoutInjectHTML = function injectHTML(icon){
                 </g>
             </svg>
         </div>`;
-        icon.parentNode.prepend(wrapper.firstChild);
+        icon.prepend(wrapper.firstChild);
         document.getElementsByName("ChannelPopout")[0].onmouseenter = ChannelPopoutOnMouseEnter;
         document.getElementsByName("ChannelPopout")[0].onmouseleave = ChannelPopoutOnMouseLeave;
         document.getElementsByName("ChannelPopout")[0].onmouseup = ChannelPopoutOnMouseClick;
@@ -66,7 +68,7 @@ const ChannelPopoutRemoveHTML = function removeHTML(){
 
 
 var ChannelPopouts = (() => {
-    const config = {"info":{"name":"ChannelPopouts","authors":[{"name":"Green","discord_id":"80593258903773184","github_username":"Curtis-D"}],"version":"1.1.4","description":"Allows you to popout DMs/Servers to view more than one DM/Server at a time.","github":"","github_raw":"https://raw.githubusercontent.com/Curtis-D/ChannelPopouts/master/ChannelPopouts.plugin.js"},"main":"index.js"};
+    const config = {"info":{"name":"ChannelPopouts","authors":[{"name":"Green","discord_id":"80593258903773184","github_username":"Curtis-D"}],"version":"1.1.5","description":"Allows you to popout DMs/Servers to view more than one DM/Server at a time.","github":"","github_raw":"https://raw.githubusercontent.com/Curtis-D/ChannelPopouts/master/ChannelPopouts.plugin.js"},"main":"index.js"};
 
     return !global.ZeresPluginLibrary ? class {
         getName() {return config.info.name;} getAuthor() {return config.info.authors.map(a => a.name).join(", ");} getDescription() {return config.info.description;} getVersion() {return config.info.version;}
@@ -86,9 +88,8 @@ var ChannelPopouts = (() => {
             Logger.log("Started");
             Library.PluginUpdater.checkForUpdate(config.info.name, config.info.version, config.info.github_raw);
             if(document.getElementsByName("Nova_Pin")[0] && !document.getElementsByName("ChannelPopout")[0]){
-                ChannelPopoutInjectHTML(document.getElementsByName("Nova_Pin")[0].parentNode);
+                ChannelPopoutInjectHTML(document.getElementsByClassName("toolbar-1t6TWx")[0]);
             }
-            
         }
 
         onStop() {
@@ -99,7 +100,7 @@ var ChannelPopouts = (() => {
         observer(e){
             if(e.addedNodes[0] && e.addedNodes[0].classList && e.addedNodes[0].getAttribute("name") === "Nova_Pin" && !document.getElementsByName("ChannelPopout")[0]){
                 let wrapper = document.createElement('div');
-                ChannelPopoutInjectHTML(e.addedNodes[0].parentNode);
+                ChannelPopoutInjectHTML(document.getElementsByClassName("toolbar-1t6TWx")[0]);
             }
         }
     };
